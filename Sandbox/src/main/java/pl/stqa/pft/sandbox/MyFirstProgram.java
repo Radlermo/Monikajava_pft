a4dbb9c9174e46b5ea3ebe2e45bd28df8c6ea8b4
Radlermo <Radler.monika@gmail.com>
2017-08-19 23:46:03 +0200

    changes in formatting settings

--- Sandbox/src/main/java/pl/stqa/pft/sandbox/MyFirstProgram.java
+++ Sandbox/src/main/java/pl/stqa/pft/sandbox/MyFirstProgram.java
@@ -2,6 +2,6 @@ package pl.stqa.pft.sandbox;
 
 public class MyFirstProgram {
     public static void main(String[] args) {
-System.out.println("hello,world");
+        System.out.println("hello,world");
     }
 }
