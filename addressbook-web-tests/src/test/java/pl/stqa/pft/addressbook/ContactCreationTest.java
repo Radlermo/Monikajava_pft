ea71cc4634ee015cc53575613ca45c09adac2887
Radlermo <Radler.monika@gmail.com>
2017-09-07 21:04:41 +0200

    contacts metody pomocnicze

--- addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/ContactCreationTest.java
+++ addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/ContactCreationTest.java
@@ -24,19 +24,32 @@ public class ContactCreationTest {
         wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true).setBinary("C:/Program Files/Mozilla Firefox/firefox.exe"));
         wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
       wd.get("http://localhost/addressbook/index.php");
-      wd.findElement(By.name("user")).click();
-      wd.findElement(By.name("user")).clear();
-      wd.findElement(By.name("user")).sendKeys("admin");
-      wd.findElement(By.name("pass")).click();
-      wd.findElement(By.name("pass")).clear();
-      wd.findElement(By.name("pass")).sendKeys("secret");
-      wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
+        login();
     }
-    
+
+    private void login() {
+        wd.findElement(By.name("user")).click();
+        wd.findElement(By.name("user")).clear();
+        wd.findElement(By.name("user")).sendKeys("admin");
+        wd.findElement(By.name("pass")).click();
+        wd.findElement(By.name("pass")).clear();
+        wd.findElement(By.name("pass")).sendKeys("secret");
+        wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
+    }
+
     @Test
     public void testContactCreation() {
 
-        wd.findElement(By.linkText("add new")).click();
+        goToContactCreation();
+        fillContactForm();
+        goToHomePage();
+    }
+
+    private void goToHomePage() {
+        wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
+    }
+
+    private void fillContactForm() {
         wd.findElement(By.name("firstname")).click();
         wd.findElement(By.name("firstname")).clear();
         wd.findElement(By.name("firstname")).sendKeys("Monika1");
@@ -53,9 +66,12 @@ public class ContactCreationTest {
         wd.findElement(By.name("email")).click();
         wd.findElement(By.name("email")).clear();
         wd.findElement(By.name("email")).sendKeys("twst@test.pl");
-        wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
     }
-    
+
+    private void goToContactCreation() {
+        wd.findElement(By.linkText("add new")).click();
+    }
+
     @AfterMethod
     public void tearDown() {
         wd.quit();
