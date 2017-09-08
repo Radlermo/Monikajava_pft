d7b4957865dae3db9058e9057c23e8d87bd71187
Radlermo <Radler.monika@gmail.com>
2017-09-08 21:35:11 +0200

    dziedziczenie TestBase, GroupDeletionTest

--- addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/GroupDeletionTests.java
@@ -15,45 +15,14 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.*;
 import static org.openqa.selenium.OutputType.*;
 
-public class GroupDeletionTests {
-    FirefoxDriver wd;
-    
-    @BeforeMethod
-    public void setUp() throws Exception {
-      /*wd = new FirefoxDriver();*/
-      wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true).setBinary("C:/Program Files/Mozilla Firefox/firefox.exe"));
-        wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-    }
-    
+public class GroupDeletionTests extends TestBase {
+
     @Test
     public void testGroupDeletion() {
-        wd.get("http://localhost/addressbook/");
-        wd.findElement(By.name("user")).click();
-        wd.findElement(By.name("user")).clear();
-        wd.findElement(By.name("user")).sendKeys("admin");
-        wd.findElement(By.name("pass")).click();
-        wd.findElement(By.name("pass")).clear();
-        wd.findElement(By.name("pass")).sendKeys("secret");
-        wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
-        wd.findElement(By.linkText("groups")).click();
-        if (!wd.findElement(By.xpath("//div[@id='content']/form/span[2]/input")).isSelected()) {
-            wd.findElement(By.xpath("//div[@id='content']/form/span[2]/input")).click();
-        }
-        wd.findElement(By.name("delete")).click();
-        wd.findElement(By.linkText("group page")).click();
+        gotoGroupPage();
+        selectGroup();
+        deleteSelectedGroup();
+        returnToGroupPage();
     }
-    
-    @AfterMethod
-    public void tearDown() {
-        wd.quit();
-    }
-    
-    public static boolean isAlertPresent(FirefoxDriver wd) {
-        try {
-            wd.switchTo().alert();
-            return true;
-        } catch (NoAlertPresentException e) {
-            return false;
-        }
-    }
+
 }
