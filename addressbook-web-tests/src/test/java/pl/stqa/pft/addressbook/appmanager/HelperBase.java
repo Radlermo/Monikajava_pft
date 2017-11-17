af287bd49ca6c9311a7ee5afaa79a865586cfbac
Radlermo <radler.monika@gmail.com>
2017-11-17 23:26:01 +0100

    6.1 Ścieżki do plików i katalogów

--- addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/appmanager/HelperBase.java
+++ addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/appmanager/HelperBase.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import java.io.File;
 
 
 public class HelperBase {
@@ -33,12 +34,18 @@ public class HelperBase {
     if (text != null) {
      String existingText = wd.findElement(locator).getAttribute("value");
       if(!text.equals(existingText)) {
-
         wd.findElement(locator).clear();
         wd.findElement(locator).sendKeys(text);
       }
     }
   }
+
+  protected void attach(By locator, File file) {
+     if (file != null) {
+       wd.findElement(locator).sendKeys(file.getAbsolutePath());
+      }
+    }
+
 
   public boolean isAlertPresent() {
     try {
--- addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/tests/ContactCreationTest.java
+++ addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/tests/ContactCreationTest.java
@@ -5,6 +5,8 @@ import org.testng.annotations.Test;
 import pl.stqa.pft.addressbook.model.ContactData;
 import pl.stqa.pft.addressbook.model.Contacts;
 
+import java.io.File;
+
 import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.*;
 import static org.testng.Assert.*;
@@ -13,13 +15,26 @@ public class ContactCreationTest extends TestBase {
 
     @Test
     public void testContactCreation() {
+      File photo = new File("src/test/resources/scrum.jpg");
       Contacts before = app.contact().allContacts();
-      ContactData contact = new ContactData().withFirstname("Monika6").withLastname("Radler8").withAddress("testowa4").withTelephonehome("754589697").withEmail("7wst@test.pl").withGroup("test2");
+      ContactData contact = new ContactData().withFirstname("Monika6").withLastname("Radler8").withAddress("testowa4").withTelephonehome("754589697").withEmail("7wst@test.pl")
+              .withGroup("test2").withPhoto(photo);
       app.contact().create(contact,true);
       Contacts after = app.contact().allContacts();
       assertThat(after.size(), equalTo(before.size() + 1));
       assertThat(after, equalTo(before.withAdded(contact.withId(after.stream().mapToInt(g -> g.getId()).max().getAsInt()))));
       /*Assert.assertEquals(new HashSet<Object>(before) , new HashSet<Object>(after));*/
     }
+// pomocniczy test, który wskazuje bieżący katalog w trakcie wykonywania testu
+    @Test
+    public void testCurrentDir() {
+        File curreantDir = new File(".");
+        System.out.println(curreantDir.getAbsolutePath());
+        File photo = new File("src/test/resources/scrum.jpg");
+        System.out.println(photo.getAbsolutePath()); // czy śceżka jest ok
+        System.out.println(photo.exists()); //czy plik istnieje
+
+    }
+
 
 }
