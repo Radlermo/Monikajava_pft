55d06dab64f24ed134698b31ab335a3881ebba63
Radlermo <radler.monika@gmail.com>
2017-11-21 12:23:17 +0100

    6.3 Analiza opcji wiersza poleceń przy generatorze danych Jcommander

--- addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/Generators/GroupDataGenerator.java
+++ addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/Generators/GroupDataGenerator.java
@@ -1,5 +1,8 @@
 package pl.stqa.pft.addressbook.Generators;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
 import pl.stqa.pft.addressbook.model.GroupData;
 import pl.stqa.pft.addressbook.model.Groups;
 
@@ -14,16 +17,37 @@ import java.util.List;
 
 public class GroupDataGenerator {
 
+    @Parameter (names = "-c", description = "Group count")
+    public int count;
+
+    @Parameter (names = "-f", description = "Target file")
+    public String file;
+
     public static void main(String[] args) throws IOException {
-        int count = Integer.parseInt(args[0]); //ilość grup
-        File file = new File(args[1]); //ścieżka do pliku
+        GroupDataGenerator generator = new GroupDataGenerator();//tworzymy nowy obiekt w bieżącej grupie
+        JCommander jCommander = new JCommander(generator);//tworzymy obiekt typu Jcommander, pierwszy objekt to obiekt który powinien mieć wypełnione atrybuty (@parameter),
+// drugi parametr to ten który jest przekazywany w wierszu poleceń
+         // int count = Integer.parseInt(args[0]); //ilość grup
+       // File file = new File(args[1]); //ścieżka do pliku
+        //może się wygenerować wyjątek więc stosujemy metodę try..catch//wywołujemy metodę parse i przekazujemy argumenty
+        try {
+            jCommander.parse(args);
+        } catch (ParameterException ex) {
+        jCommander.usage();    //wyświetlamy wyjatek na consoli
+            return;
+        }
+        generator.run();//uruchamiamy generator
 
 
-        List<GroupData> groups = generateGroups(count);//wygenerowanie danych
-        save(groups, file);     //zapisanie danych w pliku
+
     }
 
-    private static void save(List<GroupData> groups, File file) throws IOException {
+    private void run() throws IOException {
+        List<GroupData> groups = generateGroups(count);//wygenerowanie danych
+        save(groups, new File(file));     //zapisanie danych w pliku
+         }
+
+    private void save(List<GroupData> groups, File file) throws IOException {
         System.out.println(new File(".").getAbsolutePath());
         Writer writer = new FileWriter(file); //otwieramy plik do zapisu
         for (GroupData group : groups) {//przechodzimy w pętli po wszytskich grupach które znajdują się na liście groups
@@ -32,7 +56,7 @@ public class GroupDataGenerator {
         writer.close();
     }
 
-    private static List<GroupData> generateGroups(int count) {
+    private List<GroupData> generateGroups(int count) {
         List<GroupData> groups = new ArrayList<GroupData>();
         for (int i = 0; i < count; i++) {
             groups.add(new GroupData().withName(String.format("test %s", i)).withHeader(String.format("header %s", i)).withFooter(String.format("footer %s", i)));
