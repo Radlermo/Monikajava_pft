a4c079018a7c8dd5e3348825e895f4a001dbb8f6
Radlermo <radler.monika@gmail.com>
2017-11-21 13:07:56 +0100

    6.4 Parametryzacja cz1 Lista objektów

--- addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/tests/GroupCreationTest.java
+++ addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/tests/GroupCreationTest.java
@@ -1,29 +1,45 @@
 package pl.stqa.pft.addressbook.tests;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import pl.stqa.pft.addressbook.model.GroupData;
 import pl.stqa.pft.addressbook.model.Groups;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTest extends TestBase {
+    //provider testowych danych
+    @DataProvider
+    public Iterator<Object[]> validGroups() {
+        //budujemy listę tablic objektów
+        List<Object[]> list = new ArrayList<Object[]>();
+        //wypełniamy listę danymi testowymi
+        list.add(new Object[] {"test1", "header1", "footer1"}); //name, header, footer
+        list.add(new Object[] {"test2", "header2", "footer2"});
+        list.add(new Object[] {"test3", "header3", "footer3"});
+        return list.iterator();
 
-    @Test
-  public void testGroupCreation() {
+    }
 
-     app.goTo().groupPage();
-    //zbiory
-     Groups before = app.group().all();
-     GroupData group = new GroupData().withName("test19").withHeader("tet20").withFooter("test21");
-     app.group().create(group);
-     assertThat(app.group().count(), equalTo(before.size() + 1));
-     Groups after = app.group().all();
-      //group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt());  //jako parametr przyjmuje grupę, jako wynik id grupy, przesuwamy parametr do assertThat
-     //before.add(group); - usuwamy
-     assertThat(after, equalTo(before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt())))); //metoda hamcrest - sprawdza czy dwa obiekty są sobie równe. najpierw piszemy MatcherAssert.assertThat(after, CoreMatchers.equalTo(before));
-      // aby łatwiej się to czytało - żarówka/ add static import na MatcherAssert i CoreMatchers- Kod się skraca// potem przesuwamy paramter z group.withId...
-   }
+    @Test(dataProvider = "validGroups")
+    public void testGroupCreation(String name, String header, String footer) {
+            app.goTo().groupPage();
+            //zbiory
+            Groups before = app.group().all();
+            GroupData group = new GroupData().withName(name).withHeader(header).withFooter(footer);
+            app.group().create(group);
+            assertThat(app.group().count(), equalTo(before.size() + 1));
+            Groups after = app.group().all();
+            //group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt());  //jako parametr przyjmuje grupę, jako wynik id grupy, przesuwamy parametr do assertThat
+            //before.add(group); - usuwamy
+            assertThat(after, equalTo(before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt())))); //metoda hamcrest - sprawdza czy dwa obiekty są sobie równe. najpierw piszemy MatcherAssert.assertThat(after, CoreMatchers.equalTo(before));
+            // aby łatwiej się to czytało - żarówka/ add static import na MatcherAssert i CoreMatchers- Kod się skraca// potem przesuwamy paramter z group.withId...
+        }
 
 
     @Test //test sprawdzający czy niemożliwe jest stworzenie grupy z nazwą zawierającą apostrof, znak jest niedozwolony więc grupa się nie utworzy
