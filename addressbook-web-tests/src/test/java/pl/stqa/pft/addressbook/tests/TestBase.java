86c09b78741948dfb9749d240d8eaa54bb1f1fc5
Radlermo <radler.monika@gmail.com>
2017-11-24 19:32:22 +0100

    6.10 opis konfiguracji testowej podstawy failed

--- addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/tests/TestBase.java
@@ -11,7 +11,7 @@ import pl.stqa.pft.addressbook.appmanager.ApplicationManager;
 
 public class TestBase {
 
-  protected static final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
+  protected static final ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.FIREFOX));
 
 
     @BeforeSuite
